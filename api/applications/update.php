77b01ec8c56904858c6c43648cbfda89471aac38
romka <u26Jm1Tk3>
2019-03-23 20:55:07 +0300

    applications/update.php has been rewritten(order of SQL queries)

--- api/applications/update.php
+++ api/applications/update.php
@@ -12,55 +12,71 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             http_response_code(200);
             echo json_encode(['message' => 'This value is already set']);
         } else {
-            $q = $db->connection->prepare("UPDATE applications SET status=:status WHERE id=:id");
-            $q->bindParam(':id', $_POST['id']);
-            $q->bindParam(':status', $_POST['status']);
-            $res = $q->execute();
-            http_response_code(200);
-            if ($res == true && $_POST['status'] == 1) {
-                $userUpdate = $db->connection->prepare("SELECT active_projects FROM users WHERE id=:worker_id");
-                $userUpdate->bindParam(':worker_id', $checkResult->worker_id);
-                $userUpdate->execute();
-                $activeProjects = $userUpdate->fetch()[0];
-                if (count($activeProjects) < 1) {
-                    $activeProjects .= $checkResult->project_id;
-                } else {
-                    $activeProjects = explode(',', $activeProjects);
-                    array_push($activeProjects, $checkResult->project_id);
-                    $activeProjects = implode($activeProjects, ',');
-                }
-                // set project from app active for worker
-                $insertActiveProjects = $db->connection->prepare("UPDATE users SET active_projects=:ap WHERE id=:worker_id");
-                $insertActiveProjects->bindParam(':ap', $activeProjects);
-                $insertActiveProjects->bindParam(':worker_id', $checkResult->worker_id);
-                $insertActiveProjects->execute();
-
+            if ($_POST['status'] == 1 && $checkResult->status != 1) {
                 // include worker in project
                 $project = $db->connection->prepare("SELECT members FROM projects_new WHERE id=:project_id");
                 $project->bindParam(':project_id', $checkResult->project_id);
                 $project->execute();
                 $projectMembers = json_decode(($project->fetch())[0]);
                 $projectMembers = updateMembers($projectMembers, $checkResult->team, $checkResult->role, $checkResult->worker_id);
+
                 if (is_array($projectMembers)) {
                     $insertMembers = $db->connection->prepare("UPDATE projects_new SET members=:members WHERE id=:id");
                     $insertMembers->bindParam(':id', $checkResult->project_id);
                     $insertMembers->bindParam(':members', json_encode($projectMembers));
                     $insertResult = $insertMembers->execute();
+                    http_response_code(200);
                     if ($insertResult == true) {
-                        echo json_encode($projectMembers);
-                    } else {
-                        echo json_encode(['message' => false]);
+                        // set project from app active for worker
+                        $userUpdate = $db->connection->prepare("SELECT active_projects FROM users WHERE id=:worker_id");
+                        $userUpdate->bindParam(':worker_id', $checkResult->worker_id);
+                        $userUpdate->execute();
+                        $activeProjects = $userUpdate->fetch()[0];
+                        if (count($activeProjects) < 1) {
+                            $activeProjects .= $checkResult->project_id;
+                        } else {
+                            $activeProjects = explode(',', $activeProjects);
+                            array_push($activeProjects, $checkResult->project_id);
+                            $activeProjects = implode($activeProjects, ',');
+                        }
 
+                        $insertActiveProjects = $db->connection->prepare("UPDATE users SET active_projects=:ap WHERE id=:worker_id");
+                        $insertActiveProjects->bindParam(':ap', $activeProjects);
+                        $insertActiveProjects->bindParam(':worker_id', $checkResult->worker_id);
+                        $actResult = $insertActiveProjects->execute();
+                        if ($actResult == true) {
+                            // update status of application
+                            $q = $db->connection->prepare("UPDATE applications SET status=:s WHERE id=:id");
+                            $q->bindParam(':id', $_POST['id']);
+                            $q->bindParam(':s', $_POST['status']);
+                            $res = $q->execute();
+                            if ($res == true) {
+                                echo json_encode(['message' => true]);
+                            } else {
+                                echo json_encode(['message' => false, 'code' => '110']);
+                            }
+                        } else {
+                            echo json_encode(['message' => false, 'code' => '100']);
+                        }
+                    } else {
+                        echo json_encode(['message' => false, 'code' => '000']);
                     }
                 } else {
                     echo json_encode(['message' => $projectMembers]);
                 }
 
-            } else if ($res == true && $_POST['status'] == 2) {
-                echo json_encode(['message' => true]);
-
+            } else if ($_POST['status'] == 2 && $checkResult->status != 2) {
+                $q = $db->connection->prepare("UPDATE applications SET status=:s WHERE id=:id");
+                $q->bindParam(':id', $_POST['id']);
+                $q->bindParam(':s', $_POST['status']);
+                $res = $q->execute();
+                if ($res == true) {
+                    echo json_encode(['message' => true]);
+                } else {
+                    echo json_encode(['message' => false, 'code' => '000']);
+                }
             } else {
-                echo json_encode(['message' => false]);
+                echo json_encode(['message' => 'Application is already accepted/declined']);
             }
         }
     } else {
@@ -95,4 +111,15 @@ function updateMembers(array $members, int $team, string $role, int $worker_id)
         }
     }
     return $members;
+}
+
+/**
+ * @param object $application
+ * @return string
+ */
+function getUserActiveProjects(object $application)
+{
+    $db = new Database();
+
+    //return $activeProjects;
 }
