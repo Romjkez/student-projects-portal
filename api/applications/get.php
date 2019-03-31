6d22fe14a4cafe656ba7b6bab0c5658b34548fba
romka <u26Jm1Tk3>
2019-03-31 18:03:08 +0300

    Parse worker_id when get apps by project&status
    And parse project when get apps by worker_id

--- api/applications/get.php
+++ api/applications/get.php
@@ -3,14 +3,12 @@ require_once '../headers.php';
 require_once '../../database.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    if (is_numeric($_GET['worker']) && !isset($_GET['project'])) {
+    if (is_numeric($_GET['worker']) && !isset($_GET['project']) && is_numeric($_GET['page']) && is_numeric($_GET['per_page'])) {
         echo getAppsByWorkerId();
     } else if (is_numeric($_GET['status']) && is_numeric($_GET['project'])) {
         echo getAppsByStatusAndProject();
     } else if (is_numeric($_GET['workerApplied']) && is_numeric($_GET['project'])) {
         echo isWorkerRequestedJoin();
-    } else if (is_numeric($_GET['worker'])) {
-        getUserApps();
     } else {
         http_response_code(200);
         echo json_encode(['message' => 'Specify GET parameters properly']);
@@ -23,19 +21,43 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 function getAppsByWorkerId()
 {
     $db = new Database();
-    $q = $db->connection->prepare("SELECT * FROM `applications` WHERE worker_id=:worker");
+    $page = (int)$_GET['page'];
+    $per_page = (int)$_GET['per_page'];
+
+    $q = $db->connection->prepare("SELECT * FROM applications WHERE worker_id=:worker ORDER BY id DESC LIMIT :per_page OFFSET :page");
     $q->bindParam(':worker', $_GET['worker']);
+    $q->bindValue(':per_page', $per_page, PDO::PARAM_INT);
+    $q->bindValue(':page', ($page - 1) * $per_page, PDO::PARAM_INT);
     $q->execute();
     $rows = $q->rowCount();
+    $result = $q->fetchAll(PDO::FETCH_ASSOC);
     if ($rows > 0) {
-        $result = $q->fetchAll(PDO::FETCH_ASSOC);
-        http_response_code(200);
-        return json_encode($result);
+        $pagesQuery = $db->connection->prepare("SELECT id FROM applications WHERE worker_id=:worker");
+        $pagesQuery->bindParam(':worker', $_GET['worker']);
+        $pagesQuery->execute();
+        $pages = ceil($pagesQuery->rowCount() / $per_page);
+
+        $project = $db->connection->prepare("SELECT * FROM projects_new WHERE id=:id");
+        foreach ($result as &$item) {
+            $project->bindParam(':id', $item['project_id']);
+            $project->execute();
+            $item['project_id'] = $project->fetchObject();
+        }
+        return json_encode([
+            'pages' => $pages,
+            'page' => $page,
+            'per_page' => $per_page,
+            'data' => $result,
+        ]);
     } else {
         http_response_code(200);
-        return json_encode(['message' => 'No applications found']);
+        return json_encode([
+            'pages' => 0,
+            'page' => $page,
+            'per_page' => $per_page,
+            'data' => null
+        ]);
     }
-
 }
 
 function getAppsByStatusAndProject()
@@ -48,6 +70,12 @@ function getAppsByStatusAndProject()
     $rows = $q->rowCount();
     if ($rows > 0) {
         $result = $q->fetchAll(PDO::FETCH_ASSOC);
+        $userQuery = $db->connection->prepare("SELECT id,name,surname,middle_name,email,phone,stdgroup,description,avatar,usergroup,active_projects,finished_projects FROM users WHERE users.id=:id");
+        foreach ($result as &$item) {
+            $userQuery->bindParam(':id', $item['worker_id']);
+            $userQuery->execute();
+            $item['worker_id'] = $userQuery->fetchObject();
+        }
         http_response_code(200);
         return json_encode($result);
     } else {
@@ -72,15 +100,4 @@ function isWorkerRequestedJoin()
         http_response_code(200);
         return json_encode(['message' => 'false']);
     }
-}
-
-function getUserApps()
-{
-    // todo подменять иды проектов их объектами
-    $db = new Database();
-    $q = $db->connection->prepare("SELECT * FROM applications WHERE worker=:worker ORDER BY id DESC");
-    $q->bindParam(':worker', $_GET['worker']);
-    $q->execute();
-    $apps = $q->fetchAll();
-    echo $apps;
 }
