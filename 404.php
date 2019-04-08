a5b4b4dc57a796b3cbf181d3dc70f8ff0f114b12
romka <u26Jm1Tk3>
2019-04-08 23:58:29 +0300

    Editing project ready && change 404 page

--- 404.php
+++ 404.php
@@ -1,9 +1,2 @@
-<head><title>Страница не найдена</title>
-    <style>body {
-            font-family: Arial, Helvetica, sans-serif
-        }</style>
-    <meta charset="UTF-8">
-</head>
-<body>
-<h1>Страница не найдена!</h1><br><br>
-<a href="http://<?= $_SERVER['HTTP_HOST'] ?>">Вернуться на главную</a></body>
+<?php http_response_code(404);
+echo json_encode(['message' => 'Path not found']);
---
+++ api/projects/update.php
@@ -0,0 +1,57 @@
+<?php
+require_once '../headers.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (checkArguments()) {
+        require_once '../../database.php';
+        $db = new Database();
+        $tags = prepareTags($_POST['tags']);
+        $q = $db->connection->prepare("UPDATE projects_new SET title=:title, description=:description, members=:members, tags=:tags, curator=:curator, avatar=:avatar, deadline=:deadline, finish_date=:finish_date, files=:files WHERE id=:id");
+        $q->bindParam(':title', trim($_POST['title']));
+        $q->bindParam(':description', trim($_POST['description']));
+        $q->bindParam(':members', $_POST['members']);
+        $q->bindParam(':tags', $tags);
+        $q->bindParam(':curator', $_POST['curator']);
+        $q->bindParam(':avatar', $_POST['avatar']);
+        $q->bindParam(':deadline', $_POST['deadline']);
+        $q->bindParam(':finish_date', $_POST['finish_date']);
+        $q->bindParam(':files', $_POST['files']);
+        $q->bindParam(':id', $_POST['id']);
+        $result = $q->execute();
+
+        if ($result) {
+            echo json_encode(['message' => $result]);
+        } else {
+            echo json_encode(['message' => 'false']);
+        }
+    } else {
+        echo json_encode(['message' => 'Specify all the necessary parameters. Title and description must be > 2 symbols length']);
+    }
+} else {
+    http_response_code(405);
+    echo json_encode(['message' => 'Method not supported']);
+}
+
+function checkArguments()
+{
+    if (iconv_strlen(trim($_POST['title'])) > 2 && iconv_strlen(trim($_POST['description'])) > 2 && isset($_POST['id']) && isset($_POST['members'])
+        && is_numeric($_POST['curator']) && isset($_POST['avatar']) && isset($_POST['files']) && isset($_POST['deadline'])
+        && isset($_POST['finish_date']) && isset($_POST['tags'])) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function prepareTags($tags)
+{
+    $tags = explode(',', $tags, 8);
+    $result = [];
+    for ($i = 0; $i < count($tags); $i++) {
+        if (count($tags[$i]) > 0) {
+            $result[$i] = $tags[$i];
+        }
+    }
+    return implode(',', $result);
+}
+
