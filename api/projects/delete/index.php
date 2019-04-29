7f0146b2c071516691ae109a96e5d3492514e52b
Romjkez <meshkov.ra@ya.ru>
2019-04-29 13:51:23 +0300

    Checking token for project deletion in progress

--- api/projects/delete/index.php
+++ api/projects/delete/index.php
@@ -6,20 +6,20 @@ require_once '../../headers.php';
 require_once '../../../constants.php';
 require_once('../../../vendor/autoload.php');
 
-
 $headers = getallheaders();
+
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 } else if (isset($headers['X-Auth-Token'])) {
     try {
         $token = JWT::decode($headers['X-Auth-Token'], SECRET_KEY, [ALGORITHM]);
         if ($token->exp > time()) {
-            if ($token->data->usergroup == 2) {
+            if ($token->data->usergroup == 3 || FALSE) { // todo добавить проверку на куратора проекта
                 $data = updateToken($token);
                 header('X-Auth-Token: ' . JWT::encode($data, SECRET_KEY, ALGORITHM));
 
-                if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                    require_once 'create.php';
-                    create();
+                if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+                    require_once 'delete.php';
+                    delete();
                 } else {
                     http_response_code(405);
                     echo json_encode(['message' => 'Method not supported']);
